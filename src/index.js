07ee9f4e7bc49ae6e93b7afa0e729c2b002effb0
zuralt <rewardxpfreenitro@gmail.com>
2025-05-06 16:31:07 +0000

    commit

--- src/index.js
+++ src/index.js
@@ -272,9 +272,9 @@ client.on(Events.InteractionCreate, async interaction => {
 	} catch (error) {
 		console.error(error);
 		if (interaction.replied || interaction.deferred) {
-			await interaction.editReply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+			await interaction.editReply({ content: 'There was an issue while executing this command!', flags: MessageFlags.Ephemeral });
 		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+			await interaction.reply({ content: 'There was an issue while executing this command!', flags: MessageFlags.Ephemeral });
 		}
 	}
 });
