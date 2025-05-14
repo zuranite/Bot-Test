09fce10fbe368f97c7b999e7a9ed7be157b115c7
zuralt <rewardxpfreenitro@gmail.com>
2025-05-14 13:30:15 +0000

    Change msg links in welcome msg

--- src/events/guildMemberAdd/welcome.js
+++ src/events/guildMemberAdd/welcome.js
@@ -11,7 +11,7 @@ const { EmbedCreator } = require("../../modules/EmbedCreator.js")
 
 module.exports = async (client, member) => {
     const WelcomeChannel = await member.guild.channels.fetch("1372084232708554814")
-    const embed = EmbedCreator(undefined, undefined, `<a:1catjump:1359969156484829404> Welcome to Zura Works!`, ` Welcome ${member}! Enjoy your stay here! Make sure to read https://discord.com/channels/1330144654112325702/1366060433995206749. If you need any support, please refer to https://discord.com/channels/1330144654112325702/1369265586936483870. Have a nice day!`, "#fcb10f")
+    const embed = EmbedCreator(undefined, undefined, `<a:1catjump:1359969156484829404> Welcome to Zura Works!`, ` Welcome ${member}! Enjoy your stay here! Make sure to read https://discord.com/channels/1172883308087083018/1372084214207610963. If you need any support, please refer to https://discord.com/channels/1172883308087083018/1372084255504470117. Have a nice day!`, "#fcb10f")
     WelcomeChannel.send({ embeds: [embed]})
 
 }
